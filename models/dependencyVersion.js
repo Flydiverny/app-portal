98c95b61462e393067481bc22533946381a23c17
Markus Maga <markus@nadilus.com>
2016-06-21 20:13:32 +0200

    Same version can exist for different types

--- models/dependencyVersion.js
+++ models/dependencyVersion.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DependencyVersionSchema = new Schema({
-    version: { type: String, required: true, unique: true },
+    version: { type: String, required: true },
     color: { type: String, required: true, unique: true },
     type: { type: Schema.Types.ObjectId, ref: 'Dependency', required: true }
 });
