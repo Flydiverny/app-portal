146cb31253797044c4dd342759489e6700502b77
Markus Maga <markus@nadilus.com>
2016-10-10 16:03:46 +0200

    Allow multiple versions with the same name, as they might be for different apps

--- models/version.js
+++ models/version.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const VersionSchema = new Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   app: { type: Schema.Types.ObjectId, ref: 'Application', required: true },
   apk: { type: String, required: true, unique: true },
   filename: { type: String, required: true, unique: true },
