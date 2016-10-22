3c4c27d7d04b486058c761129160c50e2d17c245
Markus Maga <markus@nadilus.com>
2016-10-22 15:55:13 +0200

    feat: Respect new version model flags, released & downloadable. Some refactor and cleanup included

--- routes/index.js
+++ routes/index.js
@@ -21,35 +21,9 @@ var renderIndex = (req, res, next) => {
       }, next);
 };
 
-var resultOrError = (res) => {
-  return (result) => {
-    if (!result) return res.sendStatus(404);
-    return result;
-  }
-};
-
-var findApplication = (id, match, res) => {
-  return Application.findOne({id: id})
-    .populate({
-      path: 'versions',
-      populate: {
-        path: 'compatible',
-        options: {
-          sort: {version: -1}
-        },
-        populate: {
-          path: 'type',
-          select: 'name filterable'
-        }
-      },
-      select: 'name changelog filename compatible sortingCode downloads',
-      match: match,
-      options: {
-        sort: {sortingCode: -1}
-      }
-    }).then(resultOrError(res))
-    .then(buildDependencyList)
-    .then(buildChangelog);
+var resultOrError = (result) => {
+  if (!result) throw new Error("No result");
+  return result;
 };
 
 var buildChangelog = (app) => {
@@ -67,48 +41,78 @@ var buildChangelog = (app) => {
 
 var buildDependencyList = (app) => {
   // Build list of dependencies
-  app.dependencies = [];
-  app.versions.forEach(version => {
-    version.compatible.forEach(dep => {
-      if (dep.type.filterable) {
-        if (app.dependencies.indexOf(dep) === -1)
-          app.dependencies.push(dep);
-      }
-    });
-  });
+  app.dependencies = app.versions
+      .filter(version => !version.hidden)
+      .map(version => version.compatible.filter(dep => dep.type.filterable))
+      .reduce((a, b) => a.concat(b))
+      .filter((dep, i, self) => self.indexOf(dep) === i);
 
   return app;
 };
 
-var renderApp = function (req, res, next, showNightly, showAll, showHidden) {
+var renderApp = function (showNightly, showAll, showHidden) {
+  return (req, res, next) => {
+    queryApplication(req, res, next, showNightly)
+        .then(app => {
+          if (!showAll) {
+            var versionsCode = [];
+            var versionsToShow = [];
+
+            // Filter show only latest of each minor version.
+            app.versions.forEach(function (ver) {
+              var index = versionsCode.indexOf(Math.floor(ver.sortingCode / 100) * 100);
+              if (index === -1) {
+                if (ver.downloadable && (showHidden || !ver.hidden)) {
+                  versionsToShow.push(ver);
+                  versionsCode.push(Math.floor(ver.sortingCode / 100) * 100);
+                }
+              } else if (ver.changelog) {
+                versionsToShow[index].changelog += "\n\n" + ver.changelog;
+              }
+            });
+
+            app.versions = versionsToShow;
+          }
+
+          return app;
+        })
+        .then(app => {
+          return res.render("application", app);
+        })
+        .catch(next);
+  }
+};
+
+var queryApplication = function (req, res, next, showNightly) {
   res.locals.nightly = showNightly;
-  var query = {nightly: showNightly};
-  if (!showHidden) {
-    query.hidden = {$ne: true}
+  var query = { nightly: showNightly };
+
+  if (!res.locals.session.admin) {
+    query.released = true;
   }
 
-  findApplication(req.params.id, query, res)
-    .then(app => {
-      if (!showAll) {
-        var versionsCode = [];
-        var versionsToShow = [];
-
-        // Filter show only latest of each minor version.
-        app.versions.forEach(function (ver) {
-          var index = versionsCode.indexOf(Math.floor(ver.sortingCode / 100) * 100);
-          if (index === -1) {
-            versionsToShow.push(ver);
-            versionsCode.push(Math.floor(ver.sortingCode / 100) * 100);
-          } else if (ver.changelog) {
-            versionsToShow[index].changelog += "\n\n" + ver.changelog;
+  return Application.findOne({id: req.params.id})
+      .populate({
+        path: 'versions',
+        populate: {
+          path: 'compatible',
+          options: {
+            sort: {version: -1}
+          },
+          populate: {
+            path: 'type',
+            select: 'name filterable'
           }
-        });
-
-        app.versions = versionsToShow;
-      }
-
-      return res.render("application", app);
-    })
+        },
+        select: 'name changelog hidden filename compatible sortingCode downloads downloadable released',
+        match: query,
+        options: {
+          sort: {sortingCode: -1}
+        }
+      })
+      .then(buildDependencyList)
+      .then(buildChangelog)
+      .catch(next);
 };
 
 /* GET home page. */
@@ -128,49 +132,32 @@ router.get("/icon/:id", (req, res, next) => {
   })
 });
 
-router.get("/app/:id", (req, res, next) => {
-  renderApp(req, res, next, false, false);
-});
+router.get("/app/:id", renderApp(false, false));
 
-router.get("/app/:id/all", (req, res, next) => {
-  renderApp(req, res, next, false, true, true);
-});
+router.get("/app/:id/all", renderApp(false, true, true));
 
-router.get("/nightly/:id", (req, res, next) => {
-  renderApp(req, res, next, true, true);
-});
+router.get("/nightly/:id", renderApp(true, true));
 
 router.get("/app/:id/:deptype/:depver", (req, res, next) => {
   Dependency.findOne({name: req.params.deptype})
-    .then(resultOrError(res))
-    .then(dep => {
-      return DependencyVersion.findOne({version: req.params.depver});
-    })
-    .then(resultOrError(res))
+    .then(resultOrError)
+    .then(dep => DependencyVersion.findOne({version: req.params.depver}))
+    .then(resultOrError)
     .then(depVer => {
-      return findApplication(req.params.id, {hidden: {$ne: true}, nightly: false}, res)
-          .then(app => { return {app: app, versionId: depVer._id.toString()} });
+      return queryApplication(req, res, next, false)
+          .then(app => { return {app: app, depVer: depVer} })
     })
     .then(function (obj) {
       var app = obj.app;
-      var versionId = obj.versionId;
+      var depVerId = obj.depVer;
 
-      var versionsToShow = [];
-
-      // Filter show only latest of each minor version.
-      app.versions.forEach(ver => {
-        ver.compatible.forEach(depversion => {
-          if (depversion._id.toString() === versionId) {
-            versionsToShow.push(ver);
-            return true;
-          }
-        });
-      });
-
-      app.versions = versionsToShow;
+      app.versions = app.versions
+          .filter(ver => ver.compatible
+              .filter(depVer => depVer.equals(depVerId)).length == 1);
 
       return res.render("application", app);
-    });
+    })
+    .catch(next);
 });
 
 module.exports = router;
